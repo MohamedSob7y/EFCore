b635592dfcd26390c95087d9e1d9ac16001d9cc1
Mohamed Sobhy <moamedsobhy7@gmail.com>
2026-08-06 01:05:49 +0300

    Fluent Api Part01

--- EFCore Sessions Assignments/Session02/Context/DbCompanyContext.cs
+++ EFCore Sessions Assignments/Session02/Context/DbCompanyContext.cs
@@ -12,7 +12,9 @@ namespace EFCore_Sessions_Assignments.Context
     {
         public DbCompanyContext() : base()
         {
-           
+           //this Constructor Will Chain For Constructor Base
+           //Constructor Base Chain For Constructor Take Options
+           //Constructor Take Option take Last Override For OnConfiguration عشان يعرف الConnection String + Last Override For OnmodelCreating For Applying all configuration Using Fluent Api Method Way For Mapping 
         }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -20,12 +22,62 @@ namespace EFCore_Sessions_Assignments.Context
             optionsBuilder.UseSqlServer("Server=DESKTOP-9DG3E18;Database=CompanyDB;Trusted_Connection=true;TrustServerCertificate=true");
         }
 
+        //this Method Apply Fluent Api Mapping Configurations
+        //Fluent Api Way دى طريقة تستخدم كلها For DBValidation Not Application Validation or UiHint if You Want App Validation in this Way Use CheckConstrain
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnModelCreating(modelBuilder);
+            //دى الطريقة الاولى انى اعمل كل Configuration هنا 
+            #region Fluent Api Part01
+
+            modelBuilder.Entity<Employee>()
+                .ToTable("EmployeesTable","Sales");//Will Change Table لما يتحول فى الDb هيبقى اسمه زى دا فكك بقا من اسمه اللى موجود فى الDBSet +كمان اقدر احدد هينزل فى انهى Schema دا فى حالة ان عندى اكتر من Schema in Database 
+            //to Table Take Name of Table + Name of Schema + CheckConstrain
+            //لو الSchema مش معمولة يبقى هيعملها طب لو معمولة خلاص ضيف الTable فيها 
+            //Entity return Entity TypeBuilder
+            //=======================================================================
+            modelBuilder.Entity<Employee>().HasKey(T=>T.EmpId);//this Take FunC عشان الاسم يكون صح + Take Composit Pk لو عندى 
+            //=======================================================================
+            modelBuilder.Entity<Employee>().Property(T=>T.EmpId).UseIdentityColumn(10,10);
+            //=======================================================================
+            #region Explain Property
+            modelBuilder.Entity<Employee>().Property("Empid"); //this Take Propertyname بس فى مشكلة لو مش نفس الاسم يبقى يعمل Throw Exception
+            modelBuilder.Entity<Employee>().Property<int>("Emplid");//هنا لو موجودة بنفس الاسم يعملها عا\دى طب لو مش موجودة بنفس الاسم make this Property as Shadow Property in DB As Column عادى 
+            //يعنى Orm هيشوف لو موجودة بنفس الاسم يعملها عادى لو مش موجودة يعمل Not make Exception just make Shodow Property as Column in Db ملهاش اى representation in Application 
+            //يبقى لما اعوز اعمل column in Db ومش عايزه يتعمل فى الApplication Make this As Shadow Property
+            modelBuilder.Entity<Employee>().Property(T => T.EmpId);
+            #endregion
+            //=======================================================================
+            //modelBuilder.Entity<Employee>().Property(T=>T.EmpId).HasPrecision(10,2);
+            modelBuilder.Entity<Employee>().Property(T => T.EmpName)
+                .HasMaxLength(50);
+            //=======================================================================
+            modelBuilder.Entity<Employee>().Property(T => T.Salary)
+                .HasColumnName("EmployeeSalary")
+                .HasColumnType("decimal(10,2)");
+            //=======================================================================
+            modelBuilder.Entity<Employee>().Property(T => T.Email)
+                .IsRequired();//Not Allow Null
+
+            //modelBuilder.Entity<Employee>().Property(T => T.Email)
+            //   .IsRequired(false);//this Allow Null
+            //=======================================================================
+            //For Application Validation Make CheckConstrain
+            modelBuilder.Entity<Employee>()
+                .ToTable(T => T.HasCheckConstraint("EmailFormatConstrain", "[Email] like '%@%.%'"));//_%@_%._%  this call Regix expression 
+            modelBuilder.Entity<Employee>().Property(T => T.Password)
+                .IsRequired(false);//allow Null
+
+
+            modelBuilder.Entity<Employee>().Ignore(x => x.UserName);
+            #endregion
+            //=======================================================================
+            #region  Fluent Api Configuration
+
+
+            #endregion
         }
         #region All DbSet
-       public DbSet<Employee> Employees { get; set; }
+        public DbSet<Employee> Employees { get; set; }
         #endregion
     }
 }
--- EFCore Sessions Assignments/Session02/Models/Employee.cs
+++ EFCore Sessions Assignments/Session02/Models/Employee.cs
@@ -10,49 +10,63 @@ using System.Threading.Tasks;
 namespace EFCore_Sessions_Assignments.Session02.Models
 {
     #region Mapping Using Data Annotations
-    [Table("EmployeesTable")]//this DBValidation
-    public class Employee
-    {
-        [Key]//this DbValidation
-        public int EmpId { get; set; }
-        //==================================================
-        [MaxLength(50,ErrorMessage ="Name be 50 Character by Max")]//this Error Message when Creating object in Application Not Validation in Database 
-        [MinLength(3, ErrorMessage = "Name be 3 Character by Min")]//this   is Application Valiation تظهر when Creating object from this Class in Application 
-        [StringLength(50,MinimumLength =3,ErrorMessage ="Your string is Between 3 and 50")]//This Annotation Contain MaxLength دى الوحيدة اللى هتكون DbValidation الباقى Application Validation 
-        [Length(3,50,ErrorMessage ="Your String Must Between 50 and 3")]//this Annotation كله DbValidation When Creating Object From this يعنى كله حتى الMaxLength اللى فيه 
-        public string EmpName { get; set; }
-        //==================================================
-        [Precision(10,2)]//this DbValidation 
-        [Column("EmpSalary",TypeName ="decimal(10,2)")]//this DbValidation 
-        public decimal Salary { get; set; }
-        //==================================================
-        [Range(22,40)]//this Application Validation
-        [AllowedValues(24,26,28,30)]//Application Validation
-        [DeniedValues(20,22)]//Application Validation
-        public int Age { get; set; }
-        //==================================================
-        [Required(ErrorMessage ="You Must insert Value in Email")]//this DbValidation not allow Null
-        //but Errormessage Is ApplicationValidation not DbValidation
-        [DataType(DataType.EmailAddress)]//this UiHint For Developer
-        [EmailAddress]//this Annotation is Application Validation When Cretaing Object ولما اجى احط الEmail دا يخلينى احط الEmail With Format
-        public string Email { get; set; }//دا كدة من غير Annotation Required Not Allow Null be default
-        //public required string Email { get; set; }//this Application Validation when creating object
-        //public string? Email { get; set; }//this New Syntax For allowing Null
-        //==================================================
-        [DataType(DataType.Password)]//this UiHint For Developer
-        public string? Password { get; set; }
-        //==================================================
-        [DataType(DataType.PhoneNumber)]//this UiHint For Developer
-        [Phone]//this Application Validation عشان اكتب الformat PhoneNumber كما هو مطلوب 
-        [Required(ErrorMessage ="Must Be insert Phone Number")]
-        public int PhoneNumber { get; set; }
-        //==================================================
-        [NotMapped]//this DbValidation as this Column is AutoComputed مش هيتحول الى column in Database 
-        public string UserName =>Email.Split('@')[0];  
-            
-    }
+    //[Table("EmployeesTable")]//this DBValidation Take NameofTable + Name of Schema
+    //public class Employee
+    //{
+    //    [Key]//this DbValidation
+    //    public int EmpId { get; set; }
+    //    //==================================================
+    //    [MaxLength(50,ErrorMessage ="Name be 50 Character by Max")]//this Error Message when Creating object in Application Not Validation in Database 
+    //    [MinLength(3, ErrorMessage = "Name be 3 Character by Min")]//this   is Application Valiation تظهر when Creating object from this Class in Application 
+    //    [StringLength(50,MinimumLength =3,ErrorMessage ="Your string is Between 3 and 50")]//This Annotation Contain MaxLength دى الوحيدة اللى هتكون DbValidation الباقى Application Validation 
+    //    [Length(3,50,ErrorMessage ="Your String Must Between 50 and 3")]//this Annotation كله DbValidation When Creating Object From this يعنى كله حتى الMaxLength اللى فيه 
+    //    public string EmpName { get; set; }
+    //    //==================================================
+    //    [Precision(10,2)]//this DbValidation 
+    //    [Column("EmpSalary",TypeName ="decimal(10,2)")]//this DbValidation 
+    //    public decimal Salary { get; set; }
+    //    //==================================================
+    //    [Range(22,40)]//this Application Validation
+    //    [AllowedValues(24,26,28,30)]//Application Validation
+    //    [DeniedValues(20,22)]//Application Validation
+    //    public int Age { get; set; }
+    //    //==================================================
+    //    [Required(ErrorMessage ="You Must insert Value in Email")]//this DbValidation not allow Null
+    //    //but Errormessage Is ApplicationValidation not DbValidation
+    //    [DataType(DataType.EmailAddress)]//this UiHint For Developer
+    //    [EmailAddress]//this Annotation is Application Validation When Cretaing Object ولما اجى احط الEmail دا يخلينى احط الEmail With Format
+    //    public string Email { get; set; }//دا كدة من غير Annotation Required Not Allow Null be default
+    //    //public required string Email { get; set; }//this Application Validation when creating object
+    //    //public string? Email { get; set; }//this New Syntax For allowing Null
+    //    //==================================================
+    //    [DataType(DataType.Password)]//this UiHint For Developer
+    //    public string? Password { get; set; }
+    //    //==================================================
+    //    [DataType(DataType.PhoneNumber)]//this UiHint For Developer
+    //    [Phone]//this Application Validation عشان اكتب الformat PhoneNumber كما هو مطلوب 
+    //    [Required(ErrorMessage ="Must Be insert Phone Number")]
+    //    public int PhoneNumber { get; set; }
+    //    //==================================================
+    //    [NotMapped]//this DbValidation as this Column is AutoComputed مش هيتحول الى column in Database 
+    //    public string UserName =>Email.Split('@')[0];  
+
+    //}
 
     //ORM Make Convetion Way For Mapping First then  بعد كدة يشوف لو انا عامله اى طريقة تانيه للMapping يروح هو عامل override على الطريقة اللى عملها 
     //يعنى اول طريقة هو بيعملها هى Convetion وبعد كدة يشوف لو انا مطبق اى طريقة تانيه يعملها override 
+    #endregion
+    //========================================
+    #region using Fluent Api
+    public class Employee
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public decimal Salary { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public string? Password { get; set; }
+        public int PhoneNumber { get; set; }
+        public string UserName => Email.Split('@')[0];
+    }
     #endregion
 }
